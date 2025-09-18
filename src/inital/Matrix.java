49bb9da0bbf5c696ebe21a8b813c7182024db5b5
Julian Young <julian.young@cuw.edu>
2025-09-18 18:03:06 -0500

    Update Matrix.java

--- src/inital/Matrix.java
+++ src/inital/Matrix.java
@@ -1,5 +1,35 @@
 package inital;
 
-public class Matrix {
+public class Matrix 
+{
 
+}
+
+int MatrixSearch(int[][] arr, int key)
+{
+	int rows = arr.length;      // FOR M
+	int columns = arr[0].length; // FOR N
+	
+	int row = 0;
+	int column = columns - 1;
+}
+
+while (row < rows && col >= 0)
+{
+	if(arr[row][column] == key)
+	{
+		return new int[] {row, column};
+		
+		else if (arr[row][column] > key)
+		{
+			column--; // MOVING LEFT
+			
+			else
+			{
+				row++; // MOVING DOWN
+			}
+		}
+	}
+	
+	return new int[](-1, -1); // This is for not found, I guess
 }
